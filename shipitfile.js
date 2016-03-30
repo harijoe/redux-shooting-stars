23db3f8c0daeee8f5b35777716f80cc655db5c82
Julien Vallini <vallinij@gmail.com>
2016-03-30 22:42:38 +0200

    Add parameters

--- shipitfile.js
+++ shipitfile.js
@@ -3,18 +3,18 @@ module.exports = function (shipit) {
   require('shipit-shared')(shipit);
 
   var workspacePath = '/tmp/shooting-stars-redux';
+  var parametersFile = '.parameters';
 
   shipit.initConfig({
     default: {
       workspace: workspacePath,
       repositoryUrl: 'git@github.com:harijoe/redux-shooting-stars.git',
-      ignores: ['.git', 'node_modules', 'dist'],
+      ignores: ['.git'],
       keepReleases: 3,
       shared: {
         overwrite: true,
         dirs: [
-          'node_modules',
-          'src/config'
+          'node_modules'
         ]
       }
     },
@@ -29,6 +29,10 @@ module.exports = function (shipit) {
     return shipit.local(`cd ${workspacePath} && npm install --production`);
   };
 
+  var copyParameters = function () {
+    return shipit.local(`cp src/config/parameters.js ${workspacePath}/src/config/parameters.js`);
+  };
+
   var compile = function () {
     return shipit.local(`cd ${workspacePath} && NODE_ENV=production npm run compile`);
   };
@@ -38,7 +42,8 @@ module.exports = function (shipit) {
   });
 
   shipit.blTask('install', function () {
-    return npmInstall()
+    return copyParameters()
+      .then(npmInstall)
       .then(compile)
       .then(function () {
         shipit.log('Install Done!');
---
+++ src/config/parameters.dist.js
@@ -0,0 +1,7 @@
+let parameters = {
+  githubApiToken: 'GITHUB_SECRET_KEY',
+  homeApiHost: 'http://api.vallini.io/',
+  githubApiHost: 'https://api.github.com/'
+};
+
+export default parameters;
